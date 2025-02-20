b40140d4eed06bdf30980b7257b3af04fd20c54b
.Vi <to@cosmodrom.eu>
2025-02-21 00:39:57 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,12 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started{" "}.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+       
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="mailto:to@cosmodrom.eu"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -33,15 +28,15 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Message
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            . . .
           </a>
         </div>
       </main>
