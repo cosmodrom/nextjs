233b6f2ddf6029876a259989fbb73ad1c0080c11
.Vi <to@cosmodrom.eu>
2025-02-23 00:26:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,8 +57,8 @@ export default function Home() {
                   className="absolute text-9xl transform transition-all duration-900 hover:text-pink-500"
                   style={{
                     animation: "dance 4s ease-in infinite",
-                    top: "-90px",
-                    left: "-60px",
+                    top: "-110px",
+                    left: "-110px",
                     color: "lightgray",
                     transform: rotate(180deg),
                   }}
